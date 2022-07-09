05647e6dbef106afb629864dcd64a71bd7b8908e
Ferve <ferve91@gmail.com>
2022-07-09 20:59:46 -0300

    Agregando estrellas y estilo
    Se agrego bloques para los estilos del detalle del producto y se organizo para cuando lleguen las reviews de la BD

--- Client/src/components/Reviews/Reviews.jsx
+++ Client/src/components/Reviews/Reviews.jsx
@@ -3,64 +3,119 @@ import { useParams } from "react-router-dom";
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { postReview } from "../../redux/actions";
+import { FaStar } from "react-icons/fa"; //probando a ver si puedo ingresar las estrellas
+
+/* Variables para los colores de las estrellas */
+const colors = {
+  orange: "#FFBA5A",
+  grey: "#a9a9a9",
+};
+/* Variables para los colores de las estrellas */
 
 export const Reviews = () => {
-    const { id } = useParams();
-    const dispatch = useDispatch();
-    const product = useSelector((state) => state.detail);
-    const [comment, setComment] = useState({
-        description: '',
-        star: 0
-    });
+  /* Comienza armado de estrellas */
+  const [currentValue, setCurrentValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(undefined);
+  const stars = Array(5).fill(0);
+
+  const handleClick = (value) => {
+    setCurrentValue(value);
+  };
+
+  const handleMouseOver = (newHoverValue) => {
+    setHoverValue(newHoverValue);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverValue(undefined);
+  };
+  /* Finaliza armado de estrellas */
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const product = useSelector((state) => state.detail);
+  const [comment, setComment] = useState({
+    description: "",
+    star: 0,
+  });
   return (
     <div>
-        <div className={styles.bgPage}>
-            <div className={styles.detailContainer}>
-                <h3 className={styles.title} >Reviews</h3>
-                <div>
-                    {product[0].reviews.map((r, id) => {
-                        return (
-                            <div key={id}>
-                                <div>{r.description}</div>
-                                <div>{r.star}</div>
-                            </div>
-                        )
-                    })}                   
-                </div>
-                <h3 className={styles.title} >New Review</h3>
-                <form /* className={pte} */ onSubmit={(e) => {
-                    e.preventDefault();
-                    dispatch(postReview(comment, id))
-                    setComment({
-                        description: '',
-                        star: 0
-                    })
-                }}>
-                    <input 
-                        type='text'
-                        /* className={pte} */
-                        placeholder="What do you think about this product?" 
-                        value={comment.description}
-                        onChange={e => setComment({
-                            ...comment,
-                            description: e.target.value
-                        })}
-                    />
-                    <input 
+      <div className={styles.bgPage}>
+        <div className={styles.detailContainer}>
+          <div className={styles.reviewsContainer}>
+            <div>
+              <h3 className={styles.title}>Reviews</h3>
+            </div>
+            <div className={styles.cardsReviews}>
+              {product[0].reviews.map((r, id) => {
+                return (
+                  <div key={id}>
+                    <div>{r.description}</div>
+                    <div>{r.star}</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className={styles.newReviewContainer}>
+            <h3 className={styles.title}>New Review</h3>
+            <form
+              /* className={pte} */ onSubmit={(e) => {
+                e.preventDefault();
+                dispatch(postReview(comment, id));
+                setComment({
+                  description: "",
+                  star: 0,
+                });
+              }}
+            >
+              <input
+                type="text"
+                className={styles.input}
+                placeholder="What do you think about this product?"
+                value={comment.description}
+                onChange={(e) =>
+                  setComment({
+                    ...comment,
+                    description: e.target.value,
+                  })
+                }
+              />
+              {/* <input 
                         type='number'
-                        /* className={pte} */
+                        className={styles.input}
                         value={comment.star}
                         onChange={e => setComment({
                             ...comment,
                             star: e.target.value
                         })}
+                    /> */}
+              <div className={styles.starts}>
+                {stars.map((_, index) => {
+                  return (
+                    <FaStar
+                      key={index}
+                      size={24}
+                      onClick={() => handleClick(index + 1)}
+                      onMouseOver={() => handleMouseOver(index + 1)}
+                      onMouseLeave={handleMouseLeave}
+                      className={styles.colorEstrellas}
+                      color={
+                        (hoverValue || currentValue) > index
+                          ? colors.orange
+                          : colors.grey
+                      }
                     />
-                    <button type="submit" /* className="option-button" */>
-                        Post
-                    </button>
-                </form>
-            </div>
+                  );
+                })}
+              </div>
+
+              <button type="submit" className={styles.optionButton}>
+                Post
+              </button>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
