f96b40fa4d942e56b8f6b283b8424aabf61a5ec3
Maicol eduardo Alvarez <maicol_a_18@hotmail.com>
2022-07-11 21:05:21 -0500

    Ordering and pagination

--- Client/src/components/CardsContainer/CardsContainer.jsx
+++ Client/src/components/CardsContainer/CardsContainer.jsx
@@ -8,24 +8,46 @@ import { useDispatch, useSelector } from "react-redux";
 export const CardsContainer = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products);
+  const order = useSelector((state) => state.order);
+  const [page, setPage] = useState(1)
 
+  const previousPage = () => {
+    if(products.previousPage) {
+      setPage(page - 1)
+    }
+  }
+
+  const nextPage = () => {
+    if(products.nextPage) {
+      setPage(page + 1)
+    }
+  }
+  
   useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
+    dispatch(getProducts(page, order.type, order.by));
+  }, [dispatch, page, order]);
 
   return (
     <div className={styles.cardsContainer}>
-      <div className={styles.boxContainer}>
-        {products.map((product) => (
-          <Card
-            key={product.id}
-            id={product.id}
-            name={product.name}
-            price={product.price}
-            image={product.image}
-          />
-        ))}
+      {products.data?
+      <div>
+        <div className={styles.boxContainer}>
+          {products.data.map((product) => (
+            <Card
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              image={product.image}
+            />
+          ))}
+        </div>
+        <div className={styles.container}>
+          <button className={styles.button} onClick={()=>previousPage()}> 👈 Pagina anterior </button>
+          <button className={styles.button} onClick={()=>nextPage()}> Pagina siguiente 👉 </button>
+        </div>
       </div>
+      :'Cargando...'}
     </div>
   );
 };
--- Client/src/components/Filters/Filters.jsx
+++ Client/src/components/Filters/Filters.jsx
@@ -1,13 +1,17 @@
 import styles from "./Filters.module.css";
+import { useDispatch } from "react-redux";
+import { changeOrder } from "../../redux/actions";
 
 export const Filters = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.filtersContainer}>
       <div className={styles.filterBox}>
-        <p>Filtro 1</p>
-        <p>Filtro 2</p>
-        <p>Filtro 3</p>
-        <p>Filtro 4</p>
+        <button onClick={() => dispatch(changeOrder('ASC', 'name'))}>⏫ Ordenar de la A-Z</button>
+        <button onClick={() => dispatch(changeOrder('DESC', 'name'))}>⏬ Ordenar de la Z-A</button>
+        <button onClick={() => dispatch(changeOrder('ASC', 'price'))}>➖ Ordenar del menor al mayor precio</button>
+        <button onClick={() => dispatch(changeOrder('DESC', 'price'))}>➕ Ordenar del mayor al menor precio</button>
       </div>
     </div>
   );
