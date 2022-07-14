b0be698639b37a319ca528c9c6a16e6b32c6d863
jdmoreno2 <dario_moreno10@hotmail.com>
2022-07-13 22:15:11 -0500

    DM - Add Route User Get /:id y Put
    Se añaden las rutas Get /:id y Put de Product

--- API/src/controllers/User.js
+++ API/src/controllers/User.js
@@ -34,14 +34,13 @@ router.post("/:id/review", async (req, res) => {
     });
     res.status(200).send(reviewNew);
   } catch (e) {
-    res.status(400).send("Error: " + e)
+    return res.status(400).send("Error: " + e)
   }
 })
 
 
 router.post("/", async (req, res) => {
   try {
-
     const {
       user,
       password,
@@ -60,9 +59,63 @@ router.post("/", async (req, res) => {
     userNew.addRol(roles);
     res.status(200).send(userNew);
   } catch (e) {
-    res.status(400).send("Error: " + e)
+    return res.status(400).send("Error: " + e)
   }
 })
 
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) return res.status(400).send("Faltan datos necesarios (id).");
+    if (isNaN(parseInt(id)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (id) debe ser un numero.");
+    const user = await User.findByPk(id);
+    user ? res.status(200).json(user) : res.status(404).send({ message: "Error: El Usuario no existe" });
+  } catch (e) {
+    return res.status(400).send({ message: "Error: " + e });
+  }
+})
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, password, dni, email } = req.body;
+    if (!id) return res.status(400).send("Faltan datos necesarios (id).");
+    if (!name) return res.status(400).send("Faltan datos necesarios (name).");
+    if (!email) return res.status(400).send("Faltan datos necesarios (email).");
+    if (isNaN(parseInt(id)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (id) debe ser un numero.");
+
+    const user = await User.update(
+      {
+        name,
+        password,
+        email,
+        dni
+      },
+      {
+        where: {
+          id
+        }
+      }
+    );
+    console.log(user)
+    if (user[0] === 1) {
+      console.log('Aqui')
+      let user1 = await User.findByPk(id);
+      return res.status(200).json(user1);
+    }else if (!user){
+      return res.status(404).json({ message: "Error: El Usuario no Existe." });
+    }else{
+      return res.status(404).json({ message: "Error: Usuario no Actualizado." });
+    }
+  } catch (e) {
+    return res.status(400).send({ message: "Error: " + e });
+  }
+})
 
 module.exports = router
