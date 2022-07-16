658e36307e50a15a8d7e53b4052e1388df18a786
Ferve <ferve91@gmail.com>
2022-07-16 10:46:57 -0300

    Sacando todas las variables globales

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,20 +4,20 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-      <Auth0Provider
+      {/* <Auth0Provider
         domain="dev-f0cvoxdz.us.auth0.com"
         clientId="ANnuta1KITj5uSu9Wd7npe2uGecEtyX6"
         redirectUri={window.location.origin}
-    >
+    > */}
       <App />
-    </Auth0Provider>
+    {/* </Auth0Provider> */}
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
