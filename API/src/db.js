89bb34fc4f6492f34fc62c0af0e09aca0318f938
jdmoreno2 <dario_moreno10@hotmail.com>
2022-07-15 21:51:13 -0500

    Update db.js

--- API/src/db.js
+++ API/src/db.js
@@ -6,7 +6,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
 
 const sequelize = new Sequelize(
   // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/vinos`,
-  DATABASE_URL,
+  DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/vinos`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
