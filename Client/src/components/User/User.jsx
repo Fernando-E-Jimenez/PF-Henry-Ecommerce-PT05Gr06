e1c1c6aa8c1478f7080faf888f5234b9e27af975
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-07-19 12:18:59 -0300

    Corregi el usuario

--- Client/src/components/User/User.jsx
+++ Client/src/components/User/User.jsx
@@ -2,15 +2,13 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Admin from "../Admin/Admin";
 import styles from "./User.module.css";
-import { useAuth0 } from '@auth0/auth0-react';
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const User = () => {
   const [open, setOpen] = useState(false);
-  const {
-    isAuthenticated,
-    loginWithRedirect,
-    logout
-  } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0();
+
+  console.log(user);
 
   const handleClick = () => {
     setOpen(!open);
@@ -20,29 +18,42 @@ export const User = () => {
     <>
       <div className="relative inline-block text-left">
         <div>
-          <button
-            onClick={handleClick}
-            type="button"
-            className="inline-flex justify-center w-60 rounded-md border shadow-sm px-4 py-2 bg-white text-xl font-medium text-gray-700 hover:bg-gray-50  "
-            id="menu-button"
-            ariaexpanded="true"
-            ariahaspopup="true"
-          >
-            Administrador
-            <svg
-              className="-mr-1 ml-2 h-8 w-8"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              ariahidden="true"
+          {isAuthenticated ? (
+            <button
+              onClick={handleClick}
+              type="button"
+              className="inline-flex justify-center w-60 rounded-md border shadow-sm px-4 py-2 bg-white text-xl font-medium text-gray-700 hover:bg-gray-50  "
+              id="menu-button"
+              ariaexpanded="true"
+              ariahaspopup="true"
             >
-              <path
-                fillRule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
+              {isAuthenticated ? user.name : "Iniciar Sesion"}
+              <svg
+                className="-mr-1 ml-2 h-8 w-8"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                ariahidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          ) : (
+            <a
+              href="#"
+              className="inline-flex justify-center w-60 rounded-md border shadow-sm px-4 py-2 bg-white text-xl font-medium text-gray-700 hover:bg-gray-50"
+              role="menuitem"
+              tabIndex="-1"
+              id="menu-item-2"
+              onClick={loginWithRedirect}
+            >
+              Iniciar Sesion
+            </a>
+          )}
         </div>
 
         {open && (
@@ -63,35 +74,35 @@ export const User = () => {
               >
                 Cuenta
               </Link>
+              <p className="text-gray-700 block px-4 py-2 text-md">
+                {user.email}
+              </p>
               <hr />
-              {
-                !isAuthenticated ?
-                  <a
-                    href="#"
-                    className="text-gray-700 block px-4 py-2 text-md"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="menu-item-2"
-                    onClick={
-                      loginWithRedirect
-                    }
-                  >
-                    Iniciar Sesion
-                  </a>
-                  :
-                  <a
-                    href="#"
-                    className="text-gray-700 block px-4 py-2 text-md"
-                    role="menuitem"
-                    tabIndex="-1"
-                    id="menu-item-2"
-                    onClick={() => {
-                      logout({ returnTo: window.location.origin });
-                    }}
-                  >
-                    Salir
-                  </a>
-              }
+              {!isAuthenticated ? (
+                <a
+                  href="#"
+                  className="text-gray-700 block px-4 py-2 text-md"
+                  role="menuitem"
+                  tabIndex="-1"
+                  id="menu-item-2"
+                  onClick={loginWithRedirect}
+                >
+                  Iniciar Sesion
+                </a>
+              ) : (
+                <a
+                  href="#"
+                  className="text-gray-700 block px-4 py-2 text-md"
+                  role="menuitem"
+                  tabIndex="-1"
+                  id="menu-item-2"
+                  onClick={() => {
+                    logout({ returnTo: window.location.origin });
+                  }}
+                >
+                  Salir
+                </a>
+              )}
               {/* <a
                 href="#"
                 className="text-gray-700 block px-4 py-2 text-md"
