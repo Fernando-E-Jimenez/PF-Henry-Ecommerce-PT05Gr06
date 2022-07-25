b66c13ccb30b17d45b768b1ebd73e4e3026b702f
jdmoreno2 <dario_moreno10@hotmail.com>
2022-07-24 19:11:51 -0500

    Add Controller AdminUser
    Se crea el archivo para las rutas del Admin/User
    Junto con sus rutas Get, Post, Put y Delete

---
+++ API/src/controllers/AdminUser.js
@@ -0,0 +1,252 @@
+const { User, State, Rol } = require("../db");
+const { Router } = require("express");
+const router = Router();
+
+const userBD = async () => {
+  try {
+    return await User.findAll({
+      include: [
+        {
+          model: State
+        },
+        {
+          model: Rol
+        }
+      ]
+    });
+  } catch (e) {
+    return e;
+  }
+};
+
+// Ruta para Consultar los Usuarios Registrados
+
+router.get("/", async (req, res) => {
+  try {
+    const all = await userBD();
+    res.status(200).send(all);
+  } catch (e) {
+    return res.status(400).send({ message: "Error: " + e });
+  }
+});
+
+
+// Ruta para consultar los datos de un Usuario en especifico
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) return res.status(400).send("Faltan datos necesarios (id).");
+    if (isNaN(parseInt(id)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (id) debe ser un numero.");
+    const user = await User.findByPk(id, {
+      include: [
+        {
+          model: State
+        },
+        {
+          model: Rol
+        }
+      ]
+    });
+    if (!user) return res.status(400).send("Usuario no encontrado");
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(400).send({ message: "Error: " + error });
+  }
+});
+
+// Ruta del Admin para Crear usuarios 
+
+router.post("/", async (req, res) => {
+  try {
+    const { username, password, email, name, dni, rol, state } = req.body;
+    if (!username) return res.status(400).send("Faltan datos necesarios (username).");
+    if (!password)
+      return res.status(400).send("Faltan datos necesarios (password).");
+    if (!email) return res.status(400).send("Faltan datos necesarios (email).");
+    if (!name) return res.status(400).send("Faltan datos necesarios (name).");
+    if (!dni) return res.status(400).send("Faltan datos necesarios (dni).");
+    if (!rol) return res.status(400).send("Faltan datos necesarios (rol).");
+    if (!state) return res.status(400).send("Faltan datos necesarios (state).");
+    if (isNaN(parseInt(state)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (state) debe ser un numero.");
+    if (isNaN(parseInt(rol)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (rol) debe ser un numero.");
+    if (isNaN(parseInt(dni)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (dni) debe ser un numero.");
+    if (!isNaN(parseInt(name)))
+      return res
+        .status(400)
+        .send(
+          "Formato de datos invalido (name) debe ser una cadena texto."
+        );
+    if (!isNaN(parseInt(email)))
+      return res
+        .status(400)
+        .send(
+          "Formato de datos invalido (email) debe ser una cadena texto."
+        );
+    const userNew = await User.create({
+      username,
+      password,
+      email,
+      name,
+      dni,
+      stateId: state,
+      rolId: rol
+    });
+    return res.status(201).json(userNew);
+  } catch (e) {
+    return res.status(400).send("Error: " + e);
+  }
+});
+
+
+// Ruta para Actualizar los datos de un Usuario
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, password, email, name, dni, rol, state } = req.body;
+    if (!id) return res.status(400).send("Faltan datos necesarios (id).");
+    if (!username) return res.status(400).send("Faltan datos necesarios (username).");
+    if (!password)
+      return res.status(400).send("Faltan datos necesarios (password).");
+    if (!email) return res.status(400).send("Faltan datos necesarios (email).");
+    if (!name) return res.status(400).send("Faltan datos necesarios (name).");
+    if (!dni) return res.status(400).send("Faltan datos necesarios (dni).");
+    if (!rol) return res.status(400).send("Faltan datos necesarios (rol).");
+    if (!state) return res.status(400).send("Faltan datos necesarios (state).");
+    if (isNaN(parseInt(id)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (id) debe ser un numero.");
+    if (isNaN(parseInt(state)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (state) debe ser un numero.");
+    if (isNaN(parseInt(rol)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (rol) debe ser un numero.");
+    if (isNaN(parseInt(dni)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (dni) debe ser un numero.");
+    if (!isNaN(parseInt(name)))
+      return res
+        .status(400)
+        .send(
+          "Formato de datos invalido (name) debe ser una cadena texto."
+        );
+    if (!isNaN(parseInt(email)))
+      return res
+        .status(400)
+        .send(
+          "Formato de datos invalido (email) debe ser una cadena texto."
+        );
+
+    const user = await User.update(
+      {
+        username,
+        password,
+        email,
+        name,
+        dni,
+        stateId: state,
+        rolId: rol
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+    console.log(user);
+    if (user[0] === 1) {
+      let user1 = await User.findByPk(id, {
+        include: [
+          {
+            model: State
+          },
+          {
+            model: Rol
+          }
+        ]
+      });
+      return res.status(200).json(user1);
+    } else if (!user) {
+      return res.status(404).json({ message: "Error: El Usuario no Existe." });
+    } else {
+      return res
+        .status(404)
+        .json({ message: "Error: Usuario no Actualizado." });
+    }
+  } catch (e) {
+    return res.status(400).send({ message: "Error: " + e });
+  }
+});
+
+
+// Ruta para Eliminar y Habilitar un Usuario
+
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) return res.status(400).send("Faltan datos necesarios (id).");
+    if (isNaN(parseInt(id)))
+      return res
+        .status(400)
+        .send("Formato de datos invalido (id) debe ser un numero.");
+
+    const user = await User.findByPk(parseInt(id));
+    if (!user) return res.status(400).send("Error Usuario no encontrado.");
+
+    if (user.stateId === 1) {
+      const user1 = await User.update(
+        { stateId: 2 },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      if (user1[0] === 1) {
+        return res.status(200).json("Usuario Eliminado.");
+      } else {
+        return res.status(400).send("Error al Eliminar al Usuario.");
+      }
+    } else {
+      const user1 = await User.update(
+        { stateId: 1 },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+      if (user1[0] === 1) {
+        return res.status(200).json("Usuario Habilitado.");
+      } else {
+        return res.status(400).send("Error al Habilitar al Usuario.");
+      }
+    }
+
+  } catch (error) {
+    return res.status(400).send("Error: " + error);
+  }
+});
+
+
+module.exports = router;
--- API/src/routes/index.js
+++ API/src/routes/index.js
@@ -9,6 +9,7 @@ const adminOrder = require("../controllers/AdminOrders");
 const users = require("../controllers/User");
 const rol = require("../controllers/Rol");
 const state = require("../controllers/State");
+const adminUser = require("../controllers/AdminUser");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -22,6 +23,7 @@ router.use("/admin/category", category);
 router.use("/admin/state", state);
 router.use("/admin/rol", rol);
 router.use("/admin/order", adminOrder);
+router.use("/admin/user", adminUser);
 router.use("/guess/product", guesProduct);
 router.use("/guess/order", guesOrder);
 router.use("/guess/product", guesProductOrder);
