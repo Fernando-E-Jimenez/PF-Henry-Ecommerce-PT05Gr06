2a3a9c63294397351d89e597ccc335520823c26e
Dario Moreno <dario_moreno10@hotmail.com>
2022-07-31 10:59:46 -0500

    Revert "Merge pull request #69 from Fernando-E-Jimenez/DM-Fix-Route-Post-Product"
    This reverts commit 6446ecb96c227656a878f3d0b5c160b7c596e88b, reversing
    changes made to 4836e20100353ec69e44769d332b1c63dc886904.

--- API/src/controllers/User.js
+++ API/src/controllers/User.js
@@ -127,30 +127,6 @@ router.delete("/:idUser/car/", async (req, res) => {
 });
 
 
-// Ruta para Remover todos los Productos del carrito del usuario
-
-router.delete("/:idUser/car/reset", async (req, res) => {
-  try {
-    const { idUser } = req.params;
-    if (!idUser) return res.status(400).send("Faltan datos necesarios (idUser).");
-    if (isNaN(parseInt(idUser)))
-      return res
-        .status(400)
-        .send("Formato de datos invalido (idUser) debe ser un numero.");
-
-    const user = await User.findByPk(idUser);
-    if (!user) return res.status(400).send("El usuario no existe.");
-    const car = await user.getProducts();
-    car.map( async (p) => {
-      await user.removeProduct(p.dataValues.id);
-    })
-
-    return res.status(200).send("Productos Removidos del Carrito.");
-  } catch (error) {
-    return res.status(400).send({ message: "Error: " + error });
-  }
-});
-
 router.post("/:id/review", async (req, res) => {
   try {
     const { id } = req.params;
