7196a57f66c91ed4c72b024c377424065f71f8d7
jdmoreno2 <dario_moreno10@hotmail.com>
2022-08-03 00:26:59 -0500

    DM Add Bcrypt to Password
    Se encripta los password con bcrypt antes de actualizar o registrar usuarios

--- API/src/controllers/AdminUser.js
+++ API/src/controllers/AdminUser.js
@@ -3,6 +3,7 @@ const { transporter } = require("../utils/nodeMailer");
 const { Router } = require("express");
 const { MAILUSER } = process.env;
 const router = Router();
+const bcrypt = require('bcrypt');
 
 const userBD = async () => {
   try {
@@ -97,16 +98,23 @@ router.post("/", async (req, res) => {
         .send(
           "Formato de datos invalido (email) debe ser una cadena texto."
         );
-    const userNew = await User.create({
-      username,
-      password,
-      email,
-      name,
-      dni,
-      stateId: state,
-      rolId: rol
+
+    await bcrypt.hash(password, 10, async function (err, hash) {
+      try {
+        const userNew = await User.create({
+          username,
+          password: hash,
+          email,
+          name,
+          dni,
+          stateId: state,
+          rolId: rol
+        });
+        return res.status(201).json(userNew);
+      } catch (error) {
+        return res.status(400).send("Error: " + error);
+      }
     });
-    return res.status(201).json(userNew);
   } catch (e) {
     return res.status(400).send("Error: " + e);
   }
@@ -157,42 +165,48 @@ router.put("/:id", async (req, res) => {
           "Formato de datos invalido (email) debe ser una cadena texto."
         );
 
-    const user = await User.update(
-      {
-        username,
-        password,
-        email,
-        name,
-        dni,
-        stateId: state,
-        rolId: rol
-      },
-      {
-        where: {
-          id,
-        },
-      }
-    );
-    console.log(user);
-    if (user[0] === 1) {
-      let user1 = await User.findByPk(id, {
-        include: [
+    await bcrypt.hash(password, 10, async function (err, hash) {
+      try {
+        const user = await User.update(
           {
-            model: State
+            username,
+            password: hash1,
+            email,
+            name,
+            dni,
+            stateId: state,
+            rolId: rol
           },
           {
-            model: Rol
+            where: {
+              id,
+            },
           }
-        ]
-      });
-      return res.status(200).json(user1);
-    } else if (!user) {
-      return res.status(404).json({ message: "Error: El Usuario no Existe." });
-    } else {
-      return res
-        .status(404)
-        .json({ message: "Error: Usuario no Actualizado." });
-    }
+        );
+        console.log(user);
+        if (user[0] === 1) {
+          let user1 = await User.findByPk(id, {
+            include: [
+              {
+                model: State
+              },
+              {
+                model: Rol
+              }
+            ]
+          });
+          return res.status(200).json(user1);
+        } else if (!user) {
+          return res.status(404).json({ message: "Error: El Usuario no Existe." });
+        } else {
+          return res
+            .status(404)
+            .json({ message: "Error: Usuario no Actualizado." });
+        }
+      } catch (e) {
+        return res.status(400).send({ message: "Error: " + e });
+      }
+    });
   } catch (e) {
     return res.status(400).send({ message: "Error: " + e });
   }
@@ -358,7 +372,7 @@ router.put("/:idUser/rol", async (req, res) => {
         .status(400)
         .send("Formato de datos invalido (idUser) debe ser un numero.");
     const user = await User.findByPk(idUser);
-    if(!user) return res.status(400).send("Error Usuario no encontrado.");
+    if (!user) return res.status(400).send("Error Usuario no encontrado.");
     let idRol = 1;
     const { rolId } = user.dataValues;
     rolId === 2 ? idRol = 1 : idRol = 2;
