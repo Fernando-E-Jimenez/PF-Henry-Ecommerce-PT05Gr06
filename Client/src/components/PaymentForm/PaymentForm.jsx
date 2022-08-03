3b64fbe96d910f27060e5772b64034690f18cbb2
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-08-03 19:06:30 -0300

    Responsive mas

--- Client/src/components/PaymentForm/PaymentForm.jsx
+++ Client/src/components/PaymentForm/PaymentForm.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { corfirmPurchase } from "../../redux/actions/index";
 import { useAuth0 } from "@auth0/auth0-react";
 import { resetCartUser } from "../../redux/actions";
+import "./PaymentForm.css";
 
 function validate(post) {
   let errors = {};
@@ -27,9 +28,9 @@ export const PaymentForm = () => {
   const [errors, setErrors] = useState({});
 
   const [post, setPost] = useState({
-    name: profile.name?profile.name:'',
-    dni: '',
-    address: '',
+    name: profile.name ? profile.name : "",
+    dni: "",
+    address: "",
   });
 
   function handleInputChange(e) {
@@ -48,69 +49,84 @@ export const PaymentForm = () => {
   function handleSubmit(e) {
     e.preventDefault();
     console.log(post);
-    dispatch(corfirmPurchase(profile.id,post));
+    dispatch(corfirmPurchase(profile.id, post));
     alert("Compra confirmada continua con el proceso de pago");
     navigate("/purchases");
-    dispatch(resetCartUser(profile.id))
+    dispatch(resetCartUser(profile.id));
   }
 
   return (
-    <div>
-      
-      <h1 className="text-center text-3xl">Ya estamos muy cerca de completar la compra</h1>
+    <div className="w-4/5 sm:w-full m-auto">
+      <h1 className="text-primary-color font-bold text-center text-4xl my-10">
+        Ya estamos muy cerca de completar la compra
+      </h1>
       <hr />
-      {isAuthenticated?
-      <form onSubmit={(e) => handleSubmit(e)} className="md:w-1/2 m-auto py-6">
-          <div className="mb-5">
-          <label className="text-gray-700 font-bold text-2xl">Nombre y apellido</label>
-          <input
-            className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md text-xl"
-            type="text"
-            value={post.name}
-            name="name"
-            placeholder="Ingrese su nombre y apellido"
-            onChange={(e) => handleInputChange(e)}
-          />
-          {/* {errors.name && <p>{errors.name}</p>} */}
-        </div>
-        <div className="mb-5">
-          <label className="text-gray-700 font-bold text-2xl">Identificacion</label>
-          <input
-            className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md text-xl"
-            type="text"
-            value={post.dni}
-            name="dni"
-            placeholder="Ingrese su numero de identificacion"
-            onChange={(e) => handleInputChange(e)}
-          />
-          {/* {errors.dni && <p>{errors.dni}</p>} */}
-        </div>
-        <div className="mb-5">
-          <label className="text-gray-700 font-bold text-2xl">direccion</label>
-          <input
-            className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md text-xl"
-            type="text"
-            value={post.address}
-            name="address"
-            placeholder="Ingrese su direccion"
-            onChange={(e) => handleInputChange(e)}
-          />
-          {/* {errors.direccion && <p>{errors.direccion}</p>} */}
-        </div>
-        <button
-          className="bg-primary-color mt-10 w-full p-3 font-bold text-white rounded cursor-pointer hover:bg-sky-700 transition-colors"
-          type="submit"
+      {isAuthenticated ? (
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          className="md:w-2/6 m-auto py-6"
         >
-          Confirmar
-        </button>
-      </form>: 'Debes iniciar cesion para poder realizar un pedido'}
-      <div className="flex w-full align-middle justify-center">
+          <div className="mb-5">
+            <label className="text-gray-700 font-bold text-2xl">
+              Nombre y apellido
+            </label>
+            <input
+              className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md text-xl"
+              type="text"
+              value={post.name}
+              name="name"
+              placeholder="Ingrese su nombre y apellido"
+              onChange={(e) => handleInputChange(e)}
+            />
+            {/* {errors.name && <p>{errors.name}</p>} */}
+          </div>
+          <div className="mb-5">
+            <label className="text-gray-700 font-bold text-2xl">
+              Identificacion
+            </label>
+            <input
+              className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md text-xl"
+              type="text"
+              value={post.dni}
+              name="dni"
+              placeholder="Ingrese su numero de identificacion"
+              onChange={(e) => handleInputChange(e)}
+            />
+            {/* {errors.dni && <p>{errors.dni}</p>} */}
+          </div>
+          <div className="mb-5">
+            <label className="text-gray-700 font-bold text-2xl">
+              direccion
+            </label>
+            <input
+              className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md text-xl"
+              type="text"
+              value={post.address}
+              name="address"
+              placeholder="Ingrese su direccion"
+              onChange={(e) => handleInputChange(e)}
+            />
+            {/* {errors.direccion && <p>{errors.direccion}</p>} */}
+          </div>
+          <div className="flex justify-center mt-6">
+            <button
+              className="bg-primary-color hover:bg-secondary-color  w-2/5  p-3 font-bold text-white rounded cursor-pointer transition-colors"
+              type="submit"
+            >
+              Confirmar
+            </button>
+          </div>
+        </form>
+      ) : (
+        "Debes iniciar cesion para poder realizar un pedido"
+      )}
+      <div className="flex w-2/6 m-auto align-middle justify-center">
         <Link to="/">
-          <button className="bg-red-400 px-20 mt-20 p-3 font-bold text-white rounded cursor-pointer hover:bg-sky-700 transition-colors">
+          <button className="bg-red-400 w-56 p-3 font-bold text-white rounded cursor-pointer hover:bg-red-500 transition-colors">
             Volver
           </button>
         </Link>
       </div>
     </div>
   );
-}
+};
