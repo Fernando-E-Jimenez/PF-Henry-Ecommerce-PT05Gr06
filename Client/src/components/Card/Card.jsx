a6c94d69bd97cdd00e303dedc6776373ccc45a32
jdmoreno2 <dario_moreno10@hotmail.com>
2022-08-02 19:01:52 -0500

    DM Fix Favorite Botton
    Se cambia el icono del botón para agregar un producto a favoritos

--- Client/src/components/Card/Card.jsx
+++ Client/src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import "./Card.css";
-import { addToCart, addToCartUser, addToFavoriteUser, addToFavorite  } from "../../redux/actions";
+import { addToCart, addToCartUser, addToFavoriteUser, addToFavorite } from "../../redux/actions";
 import Swal from "sweetalert2";
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -38,9 +38,25 @@ export const Card = ({ id, name, price, image }) => {
 
   return (
     <div className="cardContainer">
-      <button className="cardFavorite" onClick={(e) => handleAddFavorite(id)}>
+      <button className="btnFavorite" onClick={(e) => handleAddFavorite(id)}>
         {
-          "❤"
+          isAuthenticated ?
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-heart"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#7e52a0"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+            </svg>
+            : null
         }
       </button>
       <Link to={`/product/${id}`}>
