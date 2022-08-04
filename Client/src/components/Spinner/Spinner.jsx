7312da4d360a1ea632c96ef11d8e9a81ed6db4c5
Tomas Dziurdzia <tomidziurdzia@gmail.com>
2022-08-04 13:31:28 -0300

    Mas responsive

---
+++ Client/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,18 @@
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="spinnerContainer">
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
