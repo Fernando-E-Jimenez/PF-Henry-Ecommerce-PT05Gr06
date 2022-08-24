77bc822490faf1a2b74ccafae52d5ee0dd40be40
Dario Moreno <dario_moreno10@hotmail.com>
2022-08-24 11:08:53 -0500

    Set Deploy
    Se modifica conexion a la base de datos y se agrega archivo para el cambio de Deploy del Back

--- API/src/db.js
+++ API/src/db.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
+const pg = require('pg');
 const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env;
 
 const sequelize = new Sequelize(
   // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/vinos`,
   DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/vinos`,
   {
+    dialectModule: pg,
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
